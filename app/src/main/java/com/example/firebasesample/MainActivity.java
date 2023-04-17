0ef29be491c9ad119634841704836b47e32f2666
Fran <jpintadosa@inspedralbes.cat>
2023-04-17 20:33:16 +0200

    Initial commit

---
+++ app/src/main/java/com/example/firebasesample/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.firebasesample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //1. Connexió a Firebase
+
+    }
+}
